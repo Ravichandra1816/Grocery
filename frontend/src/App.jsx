1e2895ed7fb95bd595068dc8e7ddd9ef4f58c80d
Ravichandra86 <ravinew711@gmail.com>
2025-04-19 13:07:04 +0530

    Login component is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,15 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
 import { Toaster } from "react-hot-toast";
 import Footer from "./Components/Footer";
+import { useAppContext } from "./Context/AppContext";
+import Login from "./Components/Login";
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
       <Toaster />
 
       <div
