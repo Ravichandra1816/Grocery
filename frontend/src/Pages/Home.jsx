2a5465b3fae386319444d41a9efdd384a42abebf
Ravichandra86 <ravinew711@gmail.com>
2025-04-19 11:23:53 +0530

    newslatter component is complete

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from "../Components/MainBanner";
 import Categories from "../Components/Categories";
 import BestSeller from "../Components/BestSeller";
 import BottomBanner from "../Components/BottomBanner";
+import NewsLatter from "../Components/NewsLatter";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Categories />
       <BestSeller />
       <BottomBanner />
+      <NewsLatter />
     </div>
   );
 };
